4045e79b4edb809ceee1f940af1f9681c0d8bc4d
Charles Melbye <charlie@mudoo.net>
2008-12-07 21:31:05 -0600

    More templates and debugging

--- controllers/GameController.php
+++ controllers/GameController.php
@@ -1,11 +1,19 @@
 <?php
 
 class GameController extends WebApp_Controller {
+	$step = 1;
+	
+	public function __construct() {
+		$this->step = substr( $_SERVER['REQUEST_URI'], 1 );
+	}
+	
 	public function get() {
+		echo $this->step;
 		WebApp_Template::render( 'game/form' );
 	}
 	
 	public function post() {
+		echo $this->step;
 		WebApp_Template::render( 'game/result' );
 	}
 }
